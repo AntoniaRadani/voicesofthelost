6722719cc0d532f126604f2bf1a524e961dda86a
Cretu Cristiana <cristiana.cretu2@student.tuiasi.ro>
2025-05-05 18:48:58 +0300

    coliziuni

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -10,62 +10,72 @@ public class CollisionChecker {
         this.gp = gp;
     }
 
-    public void checkTile(Entity entity ) {
+    private boolean isInsideMap(int row, int col) {
+        return row >= 0 && row < gp.tiledMapViewer.mapHeight &&
+                col >= 0 && col < gp.tiledMapViewer.mapWidth;
+    }
+
+
+    public void checkTile(Entity entity) {
 
         int entityLeftWorldX = entity.worldX + entity.solidArea.x;
         int entityRightWorldX = entity.worldX + entity.solidArea.x + entity.solidArea.width;
         int entityTopWorldY = entity.worldY + entity.solidArea.y;
         int entityBottomWorldY = entity.worldY + entity.solidArea.y + entity.solidArea.height;
-        // pe baza lor vom afla row, col;
-        // coodronatele varfurilor rectangluarului din interiorul playerului
 
         int entityLeftCol = entityLeftWorldX / gp.tileSize;
         int entityRightCol = entityRightWorldX / gp.tileSize;
         int entityTopRow = entityTopWorldY / gp.tileSize;
         int entityBottomRow = entityBottomWorldY / gp.tileSize;
-        // pe ce row, col se afla entity
 
-        // pentru layer walls
-        int tileNum1;
-        int tileNum2;
+        int tileNum1 = 0, tileNum2 = 0;
 
         switch (entity.direction) {
             case "up":
                 entityTopRow = (entityTopWorldY - entity.speed) / gp.tileSize;
-                tileNum1 = gp.tiledMapViewer.mapData[1][entityTopRow][entityLeftCol];
-                tileNum2 = gp.tiledMapViewer.mapData[1][entityTopRow][entityRightCol];
-                if (gp.tiledMapViewer.tileCollision[tileNum1] || gp.tiledMapViewer.tileCollision[tileNum2]) {
-                    entity.collisionOn = true;
+                if (isInsideMap(entityTopRow, entityLeftCol)) {
+                    tileNum1 = gp.tiledMapViewer.mapData[1][entityTopRow][entityLeftCol];
+                }
+                if (isInsideMap(entityTopRow, entityRightCol)) {
+                    tileNum2 = gp.tiledMapViewer.mapData[1][entityTopRow][entityRightCol];
                 }
                 break;
 
             case "down":
                 entityBottomRow = (entityBottomWorldY + entity.speed) / gp.tileSize;
-                tileNum1 = gp.tiledMapViewer.mapData[1][entityBottomRow][entityLeftCol];
-                tileNum2 = gp.tiledMapViewer.mapData[1][entityBottomRow][entityRightCol];
-                if (gp.tiledMapViewer.tileCollision[tileNum1] || gp.tiledMapViewer.tileCollision[tileNum2]) {
-                    entity.collisionOn = true;
+                if (isInsideMap(entityBottomRow, entityLeftCol)) {
+                    tileNum1 = gp.tiledMapViewer.mapData[1][entityBottomRow][entityLeftCol];
+                }
+                if (isInsideMap(entityBottomRow, entityRightCol)) {
+                    tileNum2 = gp.tiledMapViewer.mapData[1][entityBottomRow][entityRightCol];
                 }
                 break;
 
             case "left":
                 entityLeftCol = (entityLeftWorldX - entity.speed) / gp.tileSize;
-                tileNum1 = gp.tiledMapViewer.mapData[1][entityTopRow][entityLeftCol];
-                tileNum2 = gp.tiledMapViewer.mapData[1][entityBottomRow][entityLeftCol];
-                if (gp.tiledMapViewer.tileCollision[tileNum1] || gp.tiledMapViewer.tileCollision[tileNum2]) {
-                    entity.collisionOn = true;
+                if (isInsideMap(entityTopRow, entityLeftCol)) {
+                    tileNum1 = gp.tiledMapViewer.mapData[1][entityTopRow][entityLeftCol];
+                }
+                if (isInsideMap(entityBottomRow, entityLeftCol)) {
+                    tileNum2 = gp.tiledMapViewer.mapData[1][entityBottomRow][entityLeftCol];
                 }
                 break;
 
             case "right":
                 entityRightCol = (entityRightWorldX + entity.speed) / gp.tileSize;
-                tileNum1 = gp.tiledMapViewer.mapData[1][entityTopRow][entityRightCol];
-                tileNum2 = gp.tiledMapViewer.mapData[1][entityBottomRow][entityRightCol];
-                if (gp.tiledMapViewer.tileCollision[tileNum1] || gp.tiledMapViewer.tileCollision[tileNum2]) {
-                    entity.collisionOn = true;
+                if (isInsideMap(entityTopRow, entityRightCol)) {
+                    tileNum1 = gp.tiledMapViewer.mapData[1][entityTopRow][entityRightCol];
+                }
+                if (isInsideMap(entityBottomRow, entityRightCol)) {
+                    tileNum2 = gp.tiledMapViewer.mapData[1][entityBottomRow][entityRightCol];
                 }
                 break;
         }
 
+        if ((tileNum1 > 0 && gp.tiledMapViewer.tileCollision[tileNum1]) ||
+                (tileNum2 > 0 && gp.tiledMapViewer.tileCollision[tileNum2])) {
+            entity.collisionOn = true;
+        }
     }
+
 }
